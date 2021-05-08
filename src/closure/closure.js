141e91d4410d068fca47663500c18dc70df4f801
Gonzo <gvidal.industrial@gmail.com>
2021-05-08 17:27:11 -0500

    Finishing closures and lexical scope definition and exercises

--- src/closure/closure.js
+++ src/closure/closure.js
@@ -1,9 +1,9 @@
+//Example 1:
 //We're making a wallet
 const moneyBox = (coins) => {
   var saveCoins = 0;
   saveCoins += coins;
-  console.log(`MoneyBox: ${saveCoins}`);
-  
+  console.log(`MoneyBox: ${saveCoins}`); 
 }
 
 moneyBox(5);
@@ -13,6 +13,10 @@ moneyBox(10);
 // MoneyBox: 10
 
 
+
+//Example 2:
+//From an inner function making a reference to a variable in an outer function creates a closure
+//It can be recognized with a function returning a function
 const moneyBox = () => {
   let saveCoins = 0;
   const countCoins = (coins) => {
@@ -27,3 +31,5 @@ const myMoneyBox = moneyBox();
 myMoneyBox(5);
 myMoneyBox(10);
 
+// MoneyBox: 5
+// MoneyBox: 15
